6d34943091e3aa0bf6efa2ff5076ef984aae821f
KS6000 <kshura600@gmail.com>
2026-04-22 09:37:48 +0100

    Fix CI workflow using official Cypress action

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
 
   e2e: {
     baseUrl: "https://hotel-example-site.takeyaqa.dev/en-US",
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
